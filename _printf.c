cf8bb7d22bac9aacce726294f9408dfb84581efc
EzequielKH <laudarosa12@gmail.com>
2025-03-27 14:01:02 +0000

    tercer test de los script

--- _printf.c
+++ _printf.c
@@ -35,7 +35,7 @@ int _printf(const char *format, ...)
 				count += write(1, "%%", 1);
 			else
 			{
-				count += write(1, "%", 1);
+				count += write(1, "%%", 1);
 				count += write(1, format, 1);
 			}
 		}
