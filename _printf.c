9262cb1d7535977e3c975c97886c60b366509d1d
EzequielKH <laudarosa12@gmail.com>
2025-03-29 16:45:02 +0000

    quinto test de los script

--- _printf.c
+++ _printf.c
@@ -11,42 +11,42 @@
  */
 int handle_format(const char *format, va_list args)
 {
-    int count = 0;
+	int count = 0;
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-                count += print_char(va_arg(args, int));
-            else if (*format == 's')
-                count += print_string(va_arg(args, char *));
-            else if (*format == 'd' || *format == 'i')
-                count += print_int(va_arg(args, int));
-            else if (*format == 'u')
-                count += print_unsigned(va_arg(args, unsigned int));
-            else if (*format == 'o')
-                count += print_octal(va_arg(args, unsigned int));
-            else if (*format == 'x')
-                count += print_hex(va_arg(args, unsigned int), 0);
-            else if (*format == 'X')
-                count += print_hex(va_arg(args, unsigned int), 1);
-            else if (*format == 'p')
-                count += print_pointer(va_arg(args, void *));
-            else if (*format == '%')
-                count += write(1, "%", 1);
-            else
-            {
-                count += write(1, "%", 1);
-                count += write(1, format, 1);
-            }
-        }
-        else
-            count += write(1, format, 1);
-        format++;
-    }
-    return (count);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += print_char(va_arg(args, int));
+			else if (*format == 's')
+				count += print_string(va_arg(args, char *));
+			else if (*format == 'd' || *format == 'i')
+				count += print_int(va_arg(args, int));
+			else if (*format == 'u')
+				count += print_unsigned(va_arg(args, unsigned int));
+			else if (*format == 'o')
+				count += print_octal(va_arg(args, unsigned int));
+			else if (*format == 'x')
+				count += print_hex(va_arg(args, unsigned int), 0);
+			else if (*format == 'X')
+				count += print_hex(va_arg(args, unsigned int), 1);
+			else if (*format == 'p')
+				count += print_pointer(va_arg(args, void *));
+			else if (*format == '%')
+				count += write(1, "%%", 1);
+			else
+			{
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
+			}
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	return (count);
 }
 
 /**
@@ -57,15 +57,15 @@ int handle_format(const char *format, va_list args)
  */
 int _printf(const char *format, ...)
 {
-    int count;
-    va_list args;
+	int count;
+	va_list args;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
-    count = handle_format(format, args);
-    va_end(args);
+	va_start(args, format);
+	count = handle_format(format, args);
+	va_end(args);
 
-    return (count);
+	return (count);
 }
